1c769e11bb30af00920e7e8572f6607e325f221c
Roseofrap <hslorena92@gmail.com>
2023-07-04 06:02:15 -0500

    Update script.js

--- Fase 4/js/script.js
+++ Fase 4/js/script.js
@@ -1,21 +1,23 @@
-function procesaRespuestas(){
-    let total = 5
+function ProcesarRespuestas(){
+    let total = 3;
     let puntos = 0;
 
     let myForm = document.forms["formulario"];
-    let respuestascorrectas =  ["b","c","b","a","a"];
-    
-    for (let i =1; i <= total; i++) {
-        if (myForm["r" + i].value == null ||
-            myForm["r" + i].value == "") {
-            alert('favor responder todas las preguntas');
-            return false;
-        } else {
-            if (myForm["r" + i].value === respuestascorrectas[i - 1])
+    let respuestasCorrectas = ["c","a","b"];
+
+    for(let i=1;i<=total;i++){
+            if(myForm["r" + i].value == null || myForm["r" + i].value == ''){
+                alert("Por favor responda todas las preguntas");
+                return false;
+            }else{
+                if(myForm["r"+i].value === respuestasCorrectas[i+1])
+                console.log(myForm["r"+i].value);
                 puntos++;
-        }
-    }
-    let resultado = document.getElementById('resultado')
-    resultado.innerHTML='Obtuviste '+puntos+' puntos de ' + total + ' posibles ';
-    return false;
-}
+            }
+
+        } 
+      let resultado = document.getElementById('resultado')
+      resultado.innerHTML='Obtuviste '+puntos+' puntos de '+total+' posibles ';
+      return false;  
+
+ }
